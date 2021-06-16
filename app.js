fe8438e963c1927f37dc0308393f64c7d1787e4c
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-06-16 22:35:53 +0300

    routes express

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const http = require("http");
-
 const express = require('express');
 
 const app = express();
 
 // allow add new middleware
-app.use((req,res,next) => {
-	console.log('in the first middleware');	
-	next();
+// app.use((req,res,next) => {
+// 	console.log('in the first middleware');	
+// 	next();
+// })
+app.use('/add-product', (req,res,next) => {
+	res.send('<h1>add product</h1>')
 })
-app.use((req,res,next) => {
+app.use('/', (req,res,next) => {
 	console.log('in the second middleware');	
-	next();
+	res.send('<h1>Hello from Express</h1>')
 })
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+
+// alternative
+
+app.listen(3000);
--- task/index.js
+++ task/index.js
@@ -1,7 +1,31 @@
-const http = require('http');
+// const http = require('http');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
-const server = http.createServer(routes);
+// const server = http.createServer(routes);
 
-server.listen(3000);
+// server.listen(3000);
+
+
+// new task
+
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.use((req,res,next) => {
+	console.log('first middleware');
+	next();
+})
+app.use((req,res,next) => {
+	console.log('second middleware');
+	next();
+})
+app.use('/users', (req,res) => {
+	res.send('<h1>users</h1>')
+})
+app.use('/', (req,res) => {
+	res.send('<h1>home</h1>')
+})
