f504c8064ffb2beda6d57789968b602630ae4bcc
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-06-23 21:56:38 +0300

    add-product pug page

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,8 @@ const router = express.Router();
 const products = [];
 
 router.get("/add-product", (req, res, next) => {
-	res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	// res.sendFile(path.join(rootDir, "views", "add-product.html"));
+	res.render('shop');
 });
 // router.use('/product', (req, res) => {  // we can specify methods in routes
 // app.use('/product', (req, res) => {  // we can specify methods in routes
