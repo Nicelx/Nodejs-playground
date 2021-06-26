ab97aa06df1eedd3d61895b387fb4237d4512af1
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-06-26 23:37:30 +0300

    finish pug, start handlebars

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const products = [];
 
 router.get("/add-product", (req, res, next) => {
 	// res.sendFile(path.join(rootDir, "views", "add-product.html"));
-	res.render("add-product", { pageTitle: "Add Product" });
+	res.render("add-product", { pageTitle: "Add Product", path: '/admin/add-product' });
 });
 // router.use('/product', (req, res) => {  // we can specify methods in routes
 // app.use('/product', (req, res) => {  // we can specify methods in routes
