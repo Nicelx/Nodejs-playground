a68192021358a2bebbc442b544141e8345b636aa
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-06-28 23:48:29 +0300

    handlebars layout

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,13 @@ const products = [];
 
 router.get("/add-product", (req, res, next) => {
 	// res.sendFile(path.join(rootDir, "views", "add-product.html"));
-	res.render("add-product", { pageTitle: "Add Product", path: '/admin/add-product' });
+	res.render("add-product", {
+		pageTitle: "Add Product",
+		path: "/admin/add-product",
+		formsCSS: true,
+		productCSS: true,
+		activeAddProduct: true
+	});
 });
 // router.use('/product', (req, res) => {  // we can specify methods in routes
 // app.use('/product', (req, res) => {  // we can specify methods in routes
