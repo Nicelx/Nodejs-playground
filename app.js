219c4f133fedfb6ee276c844cb374fd2cedacd81
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-07-04 22:33:35 +0300

    models

--- app.js
+++ app.js
@@ -5,19 +5,10 @@ const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine(
-// 	"hbs",
-// 	expressHbs({
-// 		layoutsDir: "views/layouts/",
-// 		defaultLayout: "main-layout",
-// 		extname : 'hbs'
-// 	})
-// );
-
-// app.set('view engine', 'pug');
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -27,21 +18,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-	// res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-	res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
-
-// const server = http.createServer(app);
-
-// server.listen(3000);
-
-// alternative
-
-// allow add new middleware
-// app.use((req,res,next) => {
-// 	console.log('in the first middleware');
-// 	next();
-// })
