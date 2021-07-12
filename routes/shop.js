573201a8cf6841ee5fb6a24a29a8052ac742aa8c
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-07-12 23:12:40 +0300

    product-detail

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,18 @@
 const express = require("express");
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
 router.get("/", shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
-router.get('/orders', shopController.getOrders);
+router.get("/products/:productId", shopController.getProduct);
 
-router.get('/checkout', shopController.getCheckout);
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
