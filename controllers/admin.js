40cf6e5ffcb0a0409b91f4df7dae059f1b2b578a
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-07-17 23:31:50 +0300

    deleting cart items

--- controllers/admin.js
+++ controllers/admin.js
@@ -49,7 +49,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
 	Product.deleteById(prodId);
-	res.redirect('/')
+	res.redirect('/admin/products')
 }
 
 exports.getProducts = (req, res, next) => {
--- models/cart.js
+++ models/cart.js
@@ -5,6 +5,7 @@ const p = path.join(path.dirname(require.main.filename), "data", "cart.json");
 
 module.exports = class Cart {
 	static addProduct(id, productPrice) {
+		console.log('add cart')
 		fs.readFile(p, (err, fileContent) => {
 			let cart = {products: [], totalPrice: 0};
 			if (!err) {
@@ -25,6 +26,22 @@ module.exports = class Cart {
 			}
 			cart.totalPrice = cart.totalPrice + +productPrice;
 			fs.writeFile(p, JSON.stringify(cart), err => {
+				console.log(err);
+			})
+		})
+	}
+
+	static deleteProduct(id, productPrice) {
+		fs.readFile(p, (err, fileContent) => {
+			if (err) {
+				return;
+			}
+			const updatedCart = {...JSON.parse(fileContent)}
+			const product = updatedCart.products.find(prod => prod.id === id);
+			const productQty = product.qty;
+			updatedCart.products = updatedCart.products.filter(prod => prod.id !== id)
+			updatedCart.totalPrice -= productQty * productPrice;
+			fs.writeFile(p, JSON.stringify(updatedCart), err => {
 				console.log(err);
 			})
 		})
