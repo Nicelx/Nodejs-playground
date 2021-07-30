8e7c3c6498d3aa6b30074a2c9c1f36c08bf407ad
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-07-30 22:21:54 +0300

    create cart cart-item model and add some associations to them

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,13 +28,13 @@ exports.getProduct = (req, res, next) => {
 	// 	})
 	// })
 
-	Product.findByPk(prodId).then(product => {
+	Product.findByPk(prodId).then((product) => {
 		res.render("shop/product-detail", {
 			product: product,
 			pageTitle: product.title,
 			path: "/products",
 		});
-	})
+	});
 };
 
 exports.postCart = (req, res, next) => {
@@ -58,25 +58,21 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-	Cart.getCart((cart) => {
-		Product.fetchAll((products) => {
-			const cartProducts = [];
-			for (product of products) {
-				const cartProductData = cart.products.find((prod) => prod.id === product.id);
-				if (cartProductData) {
-					cartProducts.push({
-						productData: product,
-						qty: cartProductData.qty,
+	req.user
+		.getCart()
+		.then((cart) => {
+			return cart
+				.getProducts()
+				.then((products) => {
+					res.render("shop/cart", {
+						path: "cart",
+						pageTitle: "Your Cart",
+						products: products,
 					});
-				}
-			}
-			res.render("shop/cart", {
-				path: "cart",
-				pageTitle: "Your Cart",
-				products: cartProducts,
-			});
-		});
-	});
+				})
+				.catch((err) => console.log(err));
+		})
+		.catch((err) => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
