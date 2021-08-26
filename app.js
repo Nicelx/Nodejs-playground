c47de3a2f108ecf55ee55c1aa0ac2b5ec1492cab
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-08-26 22:27:08 +0300

    login form

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require('./routes/auth')
 
 const errorController = require("./controllers/error");
 
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
