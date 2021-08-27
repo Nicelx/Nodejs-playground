9b071a0e18bec0abbf6ab9e2281e371cac8cf9b1
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-08-27 21:50:03 +0300

    start with cookies and sessions

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require('express-session');
 
 const User = require("./models/user");
 
@@ -18,6 +19,11 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session({
+	secret : 'my secret', 
+	resave: false,
+	saveUninitialized : false,
+}));
 
 app.use((req, res, next) => {
 	User.findById("612292d82442e71f6c0fa15f")
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
 exports.getLogin = (req, res, next) => {
-			res.render("auth/login", {
-				path: "/login",
-				pageTitle: "Login",
-				isAuthenticated: req.isLoggedIn,
-			});
+	console.log(req.get('Cookie'));
+	const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1] === 'true';
+
+	res.render("auth/login", {
+		path: "/login",
+		pageTitle: "Login",
+		isAuthenticated: isLoggedIn,
+	});
 };
 
 exports.postLogin = (req, res, next) => {
-	req.isLoggedIn = true;
-	res.redirect('/')
+	// res.setHeader("Set-Cookie", "loggedIn=true");
+	req.session.isLoggedIn = true;
+	res.redirect("/");
 };
