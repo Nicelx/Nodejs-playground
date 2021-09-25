5a120a2bb6f2137bb96ab49fb43dfbf0e3c1c396
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-09-25 19:48:29 +0300

    sync, async errors, finish error handling module

--- app.js
+++ app.js
@@ -2,41 +2,49 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
 
 const User = require("./models/user");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const authRoutes = require('./routes/auth')
+const authRoutes = require("./routes/auth");
 
 const errorController = require("./controllers/error");
-const MONGODB_URI = "mongodb+srv://Nicel:12345@node-complete.nsiof.mongodb.net/shop"
+const MONGODB_URI = "mongodb+srv://Nicel:12345@node-complete.nsiof.mongodb.net/shop";
 
 const app = express();
 const store = new MongoDBStore({
-	uri : MONGODB_URI,
-	collection : 'sessions'
+	uri: MONGODB_URI,
+	collection: "sessions",
 });
-const csrfProtection = csrf()
+const csrfProtection = csrf();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-	secret : 'my secret', 
-	resave: false,
-	saveUninitialized : false,
-	store : store,
-}));
+app.use(
+	session({
+		secret: "my secret",
+		resave: false,
+		saveUninitialized: false,
+		store: store,
+	})
+);
 
 app.use(csrfProtection);
 app.use(flash());
+
+app.use((req, res, next) => {
+	res.locals.isAuthenticated = req.session.isLoggedIn;
+	res.locals.csrfToken = req.csrfToken();
+	next();
+});
 
 app.use((req, res, next) => {
 	if (!req.session.user) {
@@ -51,32 +59,29 @@ app.use((req, res, next) => {
 			next();
 		})
 		.catch((e) => {
-			throw new Error(e);
+			next(new Error(e));
 		});
-})
+});
 
-app.use((req,res,next) => {
-	res.locals.isAuthenticated = req.session.isLoggedIn;
-	res.locals.csrfToken = req.csrfToken();
-	next();
-})
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-	res.redirect('/500');
-})
+	res.status(500).render("500", {
+		pageTitle: "error",
+		path: "/500",
+		isAuthenticated: req.session.isLoggedIn,
+	});
+});
 
 mongoose
-	.connect(
-		MONGODB_URI
-	)
+	.connect(MONGODB_URI)
 	.then((result) => {
 		app.listen(3000);
 	})
