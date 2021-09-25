87c62c0eeb3aee0ab9853e02b5b166fb85028640
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-09-25 19:16:46 +0300

    500 page, express errors handling

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator/check");
 const Product = require("../models/product");
+const mongoose = require("mongoose");
 
 exports.getAddProduct = (req, res, next) => {
 	res.render("admin/edit-product", {
@@ -8,27 +9,29 @@ exports.getAddProduct = (req, res, next) => {
 		editing: false,
 		hasError: false,
 		errorMessage: null,
-		validationErrors : [],
+		validationErrors: [],
 	});
 };
 
-exports.postAddProduct = (req, res) => {
+exports.postAddProduct = (req, res, next) => {
 	const { title, imageUrl, price, description } = req.body;
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
 		return res.status(422).render("admin/edit-product", {
 			pageTitle: "Add Product",
-			path: "/admin/edit-product",
+			path: "/admin/add-product",
 			editing: false,
 			hasError: true,
 			product: { title, imageUrl, price, description },
 			errorMessage: errors.array()[0].msg,
-			validationErrors : errors.array(),
+			validationErrors: errors.array(),
 		});
 	}
 
 	const product = new Product({
+		//  to remove!
+		_id: mongoose.Types.ObjectId("614a0e2e4012371384184a95"),
 		title: title,
 		imageUrl: imageUrl,
 		price: price,
@@ -40,7 +43,12 @@ exports.postAddProduct = (req, res) => {
 		.then(() => {
 			res.redirect("/admin/products");
 		})
-		.catch((err) => console.err(err));
+		.catch((err) => {
+			// res.redirect('/500');
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -49,20 +57,26 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect("/");
 	}
 	const prodId = req.params.productId;
-	Product.findById(prodId).then((product) => {
-		if (!product) {
-			return res.redirect("/");
-		}
-		res.render("admin/edit-product", {
-			pageTitle: "Edit Product",
-			path: "/admin/edit-product",
-			editing: editMode,
-			product: product,
-			hasError: false,
-			errorMessage: null,
-			validationErrors : [],
-		}).catch((e) => console.log(e));
-	});
+	Product.findById(prodId)
+		.then((product) => {
+			if (!product) {
+				return res.redirect("/");
+			}
+			res.render("admin/edit-product", {
+				pageTitle: "Edit Product",
+				path: "/admin/edit-product",
+				editing: editMode,
+				product: product,
+				hasError: false,
+				errorMessage: null,
+				validationErrors: [],
+			});
+		})
+		.catch((err) => {
+			const error = new Error(err);
+			error.httpStatusCode = 500;
+			return next(error);
+		});
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -74,7 +88,7 @@ exports.postEditProduct = (req, res, next) => {
 	const errors = validationResult(req);
 
 	if (!errors.isEmpty()) {
-		console.log(errors.array())
+		console.log(errors.array());
 		return res.status(422).render("admin/edit-product", {
 			pageTitle: "Edit Product",
 			path: "/admin/edit-product",
@@ -85,10 +99,10 @@ exports.postEditProduct = (req, res, next) => {
 				imageUrl: updatedImageUrl,
 				price: updatedPrice,
 				description: updatedDesc,
-				_id : prodId,
+				_id: prodId,
 			},
 			errorMessage: errors.array()[0].msg,
-			validationErrors : errors.array(),
+			validationErrors: errors.array(),
 		});
 	}
 
