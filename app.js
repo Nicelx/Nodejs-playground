6f233d338e2251cf27ce2000eca19864de60d22e
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-09-28 19:39:30 +0300

    slight working piece, multer with storage config

--- app.js
+++ app.js
@@ -24,11 +24,23 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		const filenameString = new Date().valueOf() + '-' + file.originalname;
+		cb(null, filenameString);
+		// cb(null, new Date().toISOString() + '-' + file.originalname);
+	},
+})
+
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({dest: 'images'}).single('image'))
+app.use(multer({storage: fileStorage}).single('image'))
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
 	session({
