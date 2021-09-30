4e284bff545397d7e80491503b8a9b6dc9e1248a
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-09-30 19:28:49 +0300

    set headers and restrict file access

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const Product = require("../models/product");
 const Order = require("../models/order");
@@ -152,16 +152,28 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
 	const orderId = req.params.orderId;
-	const invoiceName = 'invoice-' + orderId + '.pdf';
-	const invoicePath = path.join('data', 'invoices', invoiceName)
-	fs.readFile(invoicePath, (err, data) => {
-		if (err) {
-			return next(err);
-		}
-		res.send(data)
-	})
-}
+	Order.findById(orderId)
+		.then((order) => {
+			if (!order) {
+				return next(new Error("No order found"));
+			}
+			if (order.user.userId.toString() !== req.user._id.toString()) {
+				return next(new Error("Unauthorized"));
+			}
 
+			const invoiceName = "invoice-" + orderId + ".pdf";
+			const invoicePath = path.join("data", "invoices", invoiceName);
+			fs.readFile(invoicePath, (err, data) => {
+				if (err) {
+					return next(err);
+				}
+				res.setHeader("Content-Type", "application/pdf");
+				res.setHeader("Conten-Disposition", 'inline; filename = "' + invoiceName + '"');
+				res.send(data);
+			});
+		})
+		.catch((e) => next(e));
+};
 
 // exports.getCheckout = (req, res, next) => {
 // 	res.render("shop/checkout", {
