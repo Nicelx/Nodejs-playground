f5268b0ffc49269bb9c7d4337e210e01cc3f3546
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-10-15 23:28:59 +0300

    storing posts in db

--- rest-backend/app.js
+++ rest-backend/app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const feedRoutes = require('./routes/feed');
+const feedRoutes = require("./routes/feed");
 
 const app = express();
+
+const MONGODB_URI = "mongodb+srv://Nicel:12345@node-complete.nsiof.mongodb.net/messages";
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+mongoose
+	.connect(MONGODB_URI)
+	.then((result) => {
+		app.listen(8080);
+	})
+	.catch((err) => console.log(err));
