40829063eae79f761f0afe63b5be257d9ef167e5
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-10-17 21:26:17 +0300

    upload image

--- rest-backend/app.js
+++ rest-backend/app.js
@@ -1,17 +1,41 @@
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "images");
+	},
+	filename: (req, file, cb) => {
+		const filenameString = new Date().valueOf() + '-' + file.originalname;
+		cb(null, filenameString);
+	},
+});
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === "image/png" ||
+		file.mimetype === "image/jpg" ||
+		file.mimetype === "image/jpeg"
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
 const MONGODB_URI = "mongodb+srv://Nicel:12345@node-complete.nsiof.mongodb.net/messages";
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
@@ -23,12 +47,12 @@ app.use((req, res, next) => {
 app.use("/feed", feedRoutes);
 app.use((err, req, res, next) => {
 	console.log(err);
-	const status = err.statusCode || 500
+	const status = err.statusCode || 500;
 	const message = err.message;
 	res.status(status).json({
-		message
+		message,
 	});
-})
+});
 
 mongoose
 	.connect(MONGODB_URI)
