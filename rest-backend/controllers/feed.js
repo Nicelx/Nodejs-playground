b0703b6c64bcd484f2cbe45946c32a2fdcf89eb6
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-10-18 23:16:13 +0300

    update post

--- rest-backend/controllers/feed.js
+++ rest-backend/controllers/feed.js
@@ -1,14 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const { validationResult } = require("express-validator/check");
 
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
 	Post.find()
-		.then(posts => {
+		.then((posts) => {
 			res.status(200).json({
-				message: 'fetched posts succesfully',
-				posts
-			})
+				message: "fetched posts succesfully",
+				posts,
+			});
 		})
 		.catch((err) => {
 			if (!err.statusCode) {
@@ -26,12 +28,12 @@ exports.createPost = (req, res, next) => {
 		throw error;
 	}
 	if (!req.file) {
-		const error = new Error('No image provided');
+		const error = new Error("No image provided");
 		error.statusCode = 422;
 		throw error;
 	}
 
-	const imageUrl = req.file.path.replace("\\" ,"/");
+	const imageUrl = req.file.path.replace("\\", "/");
 	const title = req.body.title;
 	const content = req.body.content;
 	const post = new Post({
@@ -79,3 +81,60 @@ exports.getPost = (req, res, next) => {
 			next(err);
 		});
 };
+
+exports.updatePost = (req, res, next) => {
+	const postId = req.params.postId;
+
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error("Validation failed");
+		error.statusCode = 422;
+		throw error;
+	}
+
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.image;
+
+	if (req.file) {
+		imageUrl = req.path.replace("\\", "/");
+	}
+
+	if (!imageUrl) {
+		const error = new Error("No file picked");
+		error.statusCode = 422;
+		throw error;
+	}
+	Post.findById(postId)
+		.then((post) => {
+			if (!post) {
+				const error = new Error("Not find post");
+				error.statusCode = 404;
+				throw error;
+			}
+			if (imageUrl !== post.imageUrl) {
+				clearImage(post.imageUrl)
+			}
+			post.title = title;
+			post.imageUrl = imageUrl;
+			post.content = content;
+			return post.save();
+		})
+		.then((result) => {
+			res.status(200).json({ message: "post updated" , post : result});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
+
+const clearImage = filePath => {
+	const fP = path.join(__dirname, '..', filePath) 
+	fs.unlink(fP, err => {
+		console.log(err)
+	})
+}
