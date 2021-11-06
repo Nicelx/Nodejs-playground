c3eee3f4c13dcff0a350598e29adb1dd664627c5
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-11-06 21:24:10 +0300

    mutation resolver, discover graphiQL interface

--- rest-backend/app.js
+++ rest-backend/app.js
@@ -51,6 +51,7 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHttp({
 	schema : graphqlSchema,
 	rootValue : graphqlResolver,
+	graphiql : true
 }));
 
 
--- rest-backend/graphql/resolvers.js
+++ rest-backend/graphql/resolvers.js
@@ -1,3 +1,21 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 module.exports = {
-	
-}
+	createUser: async function ({ userInput }, req) {
+		const { email, password, name } = userInput;
+		const existingUser = await User.findOne({ email: email });
+		if (existingUser) {
+			const error = new Error("User exists already");
+			throw error;
+		}
+		const hashedPw = await bcrypt.hash(password, 12);
+		const user = new User({
+			email,
+			name,
+			password: hashedPw,
+		});
+		const createdUser = await user.save();
+		return { ...createdUser._doc, _id: createdUser._id.toString() };
+	},
+};
