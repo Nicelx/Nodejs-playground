63f6f928001a669a10f5310714a8bfc895331bd4
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-11-09 21:08:39 +0300

    graphql login

--- rest-backend/graphql/resolvers.js
+++ rest-backend/graphql/resolvers.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
-const validator = require('validator');
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -7,13 +8,16 @@ module.exports = {
 	createUser: async function ({ userInput }, req) {
 		const errors = [];
 		if (!validator.isEmail(userInput.email)) {
-			errors.push({message: 'E-mail is invalid'});
+			errors.push({ message: "E-mail is invalid" });
 		}
-		if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min : 5})) {
-			error.push({message : 'password is short'})
+		if (
+			validator.isEmpty(userInput.password) ||
+			!validator.isLength(userInput.password, { min: 5 })
+		) {
+			error.push({ message: "password is short" });
 		}
 		if (errors.length > 0) {
-			const error = new Error('invalid input');
+			const error = new Error("invalid input");
 			error.data = errors;
 			error.code = 422;
 			throw error;
@@ -32,5 +36,31 @@ module.exports = {
 		});
 		const createdUser = await user.save();
 		return { ...createdUser._doc, _id: createdUser._id.toString() };
+	},
+	login: async function ({ email, password }) {
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			const error = new Error("User not found");
+			error.code = 401;
+			throw error;
+		}
+		const isEqual = await bcrypt.compare(password, user.password);
+		if (!isEqual) {
+			const error = new Error("Password is incorrect.");
+			error.code = 401;
+			throw error;
+		}
+		const token = jwt.sign(
+			{
+				userId: user._id.toString(),
+				email: user.email,
+			},
+			"secret",
+			{ expiresIn: "1h" }
+		);
+		return {
+			token,
+			userId: user._id.toString()
+		}
 	},
 };
