1218cacc9e622225858afde1c4d587c9b84bce95
Nikolay Gorshkov <37116427+Nicelx@users.noreply.github.com>
2021-12-02 21:19:44 +0300

    tests with db

--- tests/test/auth-controller.js
+++ tests/test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 const sinon = require("sinon");
+const mongoose = require("mongoose");
 
 const User = require("../models/user");
 const AuthController = require("../controllers/auth");
@@ -11,15 +12,54 @@ describe("Auth Controller - Login", function () {
 
 		const req = {
 			body: {
-				email: 'test@test.com',
-				password: 'sdfdsf',
-			}
+				email: "test@test.com",
+				password: "sdfdsf",
+			},
 		};
-		AuthController.login(req, {}, () => {}).then(result => {
-			expect(result).to.be.an('error');
-			expect(result).to.have.property('statusCode', 500);
+		AuthController.login(req, {}, () => {}).then((result) => {
+			expect(result).to.be.an("error");
+			expect(result).to.have.property("statusCode", 500);
 			done();
 		});
 		User.findOne.restore();
+	});
+
+	it("should send a response with a valid user status for an existing user", function (done) {
+		const MONGODB_URI = "mongodb+srv://Nicel:12345@node-complete.nsiof.mongodb.net/test-messages";
+
+		mongoose
+			.connect(MONGODB_URI)
+			.then((result) => {
+				const user = new User({
+					email: 'test@test.com',
+					password: 'tester',
+					name: 'test',
+					posts: [],
+					_id: '5c0f66b979af55031b34728a',
+				}); 
+				return user.save();
+			})
+			.then(() => {
+				const req = {
+					userId: '5c0f66b979af55031b34728a'
+				};		
+				const res = {
+					statusCode: 500,
+					userStatus: null,
+					status: function(code) {
+						this.statusCode = code;
+						return this;
+					},
+					json: function(data) {
+						this.userStatus = data.status
+					}
+				}	
+				AuthController.getUserStatus(req, res, () => {}).then(() => {
+					expect(res.statusCode).to.be.equal(200);
+					expect(res.userStatus).to.be.equal('I am new!!')
+					done();
+				})	
+			})
+			.catch((err) => console.log(err));
 	});
 });
